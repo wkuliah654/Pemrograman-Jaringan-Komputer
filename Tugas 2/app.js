88c07c313676ef3d6829861d535fbd973cd6d8de
wandaprogram654 <wandaprogram654@gmail.com>
2022-08-31 23:47:25 +0700

    tugas 2 31/08/2022

---
+++ Tugas 2/app.js
@@ -0,0 +1,20 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {})
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+	socket.on('send', param => {
+		console.log(param.key + ' => ' + param.value)
+		// io.emit()
+	})
+	socket.on('recive', param => {
+		console.log(param.key + ': ' + param.value)
+		// io.emit()
+	})
+})
+
+server.listen(3000)
