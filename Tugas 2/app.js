13260fab3ce1c6d31397e4e97e39684165e26ffe
wandaprogram654 <wandaprogram654@gmail.com>
2022-09-02 05:23:30 +0700

    Tugas 2 02/09/2022

--- Tugas 2/app.js
+++ Tugas 2/app.js
@@ -3,7 +3,7 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server, {})
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html')
+	res.sendFile(__dirname + '/app.html')
 })
 
 io.on('connection', socket => {
